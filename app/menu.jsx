23380824886bd1cefe57bcfb3d8507eb2b6562b3
anasgee <anasraza6096@gmail.com>
2025-02-15 01:12:49 +0500

    updated to menu styling, learned FlatList basiscs

--- app/menu.jsx
+++ app/menu.jsx
@@ -2,6 +2,8 @@ import { Appearance, StyleSheet,Platform,SafeAreaView, ScrollView,View,Text,Imag
 
 import { Colors } from "@/constants/Colors";
 import { FlatList } from "react-native-gesture-handler";
+import { MENU_ITEMS } from "@/constants/MenuItems";
+import MenuImages from "@/constants/MenuImages";
 
 export default function MenuScreen(){
     const colorScheme = Appearance.getColorScheme();
@@ -10,14 +12,32 @@ export default function MenuScreen(){
 
     const Container = Platform.OS === "web" ? ScrollView : SafeAreaView;
 
+    const styles = createStyle(theme,colorScheme)
+    const separatorComp = <View style={styles.separator}/>
+    const headerComp = <Text>Top Of the list</Text>
+    const footerComp = <Text>End Of the list</Text>
 
     return(
         <Container>
             <FlatList
-            data = {[]}
-            renderItem={({item})=>{
-
-            }}
+            contentContainerStyle={styles.contentContainer}
+            data = {MENU_ITEMS}
+            ItemSeparatorComponent={separatorComp}
+            ListHeaderComponent={headerComp}
+            ListFooterComponent={footerComp}
+            ListFooterComponentStyle={styles.footerComp}
+            // ListHeaderComponentStyle={""}
+            showsVerticalScrollIndicator={false}
+            keyExtractor={(item)=>item.id.toString()}   
+            renderItem={({item})=>(
+                    <View>
+                        <View>
+                            <Text>{item.title} </Text>
+                            <Text>{item.description} </Text>
+                        </View>
+                        <Image source={MenuImages[item.id - 1]}/>
+                    </View>
+            )}/>
         </Container>
     )
 
@@ -26,6 +46,22 @@ export default function MenuScreen(){
 
 function createStyle(theme,colorScheme){
  return StyleSheet.create({
-
+            contentContainer:{
+                paddingTop:10,
+                paddingBottom:20,
+                paddingHorizontal:12,
+                backgroundColor:theme.background
+            },
+            separator:{
+                height:1,
+                backgroundColor:colorScheme==="dark" ? "papayawhip":"#000",
+                width:'50%',
+                maxWidth:300,
+                marginHorizontal:'auto',
+                marginBottom:10
+                        },
+                        footerComp:{
+                            marginHorizontal:'auto'
+                        }
  })
 }
---
+++ constants/MenuImages.js
@@ -0,0 +1,14 @@
+import one from "@/assets/images/chai/1.png";
+import two from "@/assets/images/chai/2.png"
+import three from "@/assets/images/chai/3.png"
+import four from "@/assets/images/chai/4.png"
+import five from "@/assets/images/chai/5.png"
+import siz from "@/assets/images/chai/6.png"
+import seven from "@/assets/images/chai/7.png"
+import eight from "@/assets/images/chai/8.png"
+import nine from "@/assets/images/chai/9.png"
+import ten from "@/assets/images/chai/10.png"
+
+export default [
+    one,two,three,four,five,seven,siz,eight,nine,ten
+]
